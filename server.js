266ff44c8f5db63b6390529bb387fdc40aa5fc65
R-A-exe <royatalla.ra@gmail.com>
2021-02-23 15:28:26 -0500

    Fixing routing error

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const mysql = require('mysql');
-
+const exphbs = require('express-handlebars');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -10,6 +10,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.use(require('./model/burger'));
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+app.use('/*',require('./controllers/burgers_controller'));
 
 app.listen(PORT, console.log(`Listening to port ${PORT}`));
