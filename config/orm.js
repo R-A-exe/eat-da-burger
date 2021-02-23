a5c39cf462c339512d515c0024f9e21902f04e2f
R-A-exe <royatalla.ra@gmail.com>
2021-02-23 14:19:44 -0500

    Added orm.js

---
+++ config/orm.js
@@ -0,0 +1,33 @@
+var connection = require('./connection');
+
+var orm = {
+    selectAll: function () {
+        return new Promise(async function (resolve, reject) {
+            connection.query('SELECT * FROM burgers', (err, data) => {
+                if (err) return (reject(err));
+                return (resolve(data));
+            });
+        });
+    },
+
+    insertOne: function (name) {
+        return new Promise(async function (resolve, reject) {
+            connection.query('INSERT INTO burgers VALUES(default, ? , false', name, (err) => {
+                if (err) return (reject(err));
+                return (resolve(true));
+            });
+        });
+    },
+
+    updateOne: function (id) {
+        return new Promise(async function (resolve, reject) {
+            connection.query('UPDATE burgers SET devoured = true WHERE id = ?', id, (err) => {
+                if (err) return (reject(err));
+                return (resolve(true));
+            });
+        });
+    }
+}
+
+
+module.exports = orm;
