4453885c5f9a5b087b7201762c27f7658d5182ed
R-A-exe <royatalla.ra@gmail.com>
2021-02-23 13:58:31 -0500

    Added seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers VALUES (default, 'first burger', true);
+INSERT INTO burgers VALUES (default, 'second burger', true);
+INSERT INTO burgers VALUES (default, 'third burger', false);
+INSERT INTO burgers VALUES (default, 'fourth burger', false);
