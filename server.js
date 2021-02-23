c6a8d83c9cc176862dc8714ed7d1d988abc3f4e5
R-A-exe <royatalla.ra@gmail.com>
2021-02-23 15:20:14 -0500

    Added server routes and middleware

--- server.js
+++ server.js
@@ -1,31 +1,15 @@
 const express = require ('express');
 const mysql = require('mysql');
-const exphbs = require('express-handlebars');
 
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use('/api', require('./model/burger'));
 
 app.listen(PORT, console.log(`Listening to port ${PORT}`));
